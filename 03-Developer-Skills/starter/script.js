5f0cbf6b42610dfbb5c31b7e6e6fef336a3a96d4
Jorge Alberto Rodriguez Flores <jarf4321@gmail.com>
2020-10-29 23:23:56 -0600

    Agregamos una configuracion personalizada de prettier

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,15 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const measureKelvin = () => {
+  const measurument = {
+    type: 'temp',
+    unit: 'cels',
+    value: prompt('Degrees celsius'),
+  };
+
+  console.log(measurument.value);
+  const kelvin = Number(measurument.value) + 273;
+  return kelvin;
+};
+console.log(measureKelvin());
